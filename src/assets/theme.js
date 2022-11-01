37b1253687c41136ef001ae6bcd347244503638c
Mateusz Stopa <mateusz.stopa001@gmail.com>
2022-11-01 10:52:49 +0100

    basic setup

---
+++ src/assets/theme.js
@@ -0,0 +1,20 @@
+export const theme = {
+  colors: {
+    white: "#FFFFFF",
+    black: "#111111",
+  },
+  fontSize: {
+    xxl: "34px",
+    xl: "24px",
+    l: "17px",
+    m: "13px",
+    s: "11px",
+  },
+  mediaQuery: {
+    mobileL: `(min-width: 400px)`,
+    tablet: `(min-width: 768px)`,
+    laptop: `(min-width: 1024px)`,
+    desktop: `(min-width: 1400px)`,
+    desktopL: `(min-width: 2000px)`,
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GlobalStyle } from "./assets/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./assets/theme";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
